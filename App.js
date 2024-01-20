484ee3a334cdfaa00c3bea7df447fa10e22f6dec
imshubsoni <imshubsoni@gmail.com>
2024-01-20 17:51:33 +0530

    React Element, React Component, Component Composition

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-var heading = React.createElement("h1", {}, "Hello World from ReactJS");
-
-var parentChild = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hello World from ReactJS"),
-    React.createElement("h2", {}, "H2 from ReactJS"),
-  ])
+// React Element ->
+const heading = (
+  <h1 id="heading" className="heading">
+    This is React from JSX
+  </h1>
 );
+
+// React Functional Component -> (Name starts with Capital Letter)
+const Heading = () => {
+  return (
+    <h1 id="heading2" className="heading2">
+      This is functional Component
+    </h1>
+  );
+};
+
+// Component Composition -> Nesting one Component into Another
+const Page = () => {
+  return (
+    <div id="page">
+      <Heading />
+      <p className="desc">This is page description</p>
+    </div>
+  );
+};
 
 var root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parentChild);
+// Render Element
+// root.render(heading);
+
+// Render Component
+root.render(<Page />);
