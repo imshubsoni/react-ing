56329cb1c53fc5263bb765b54e88a96c5bc4e0b5
imshubsoni <imshubsoni@gmail.com>
2024-01-23 22:14:34 +0530

    Food Ordering App - Adding multiple components using live data

--- AppLayout.js
+++ AppLayout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import resList from "./RestaurantsList";
 
 const Header = () => {
   return (
@@ -21,65 +22,6 @@ const Header = () => {
       </div>
     </div>
   );
-};
-
-const resData = {
-  info: {
-    id: "65797",
-    name: "Leon's - Burgers & Wings (Leon Grill)",
-    cloudinaryImageId: "b2edbc28b7b8219d6e0a9c049ce06658",
-    locality: "Koramangala",
-    areaName: "Koramangala",
-    costForTwo: "₹300 for two",
-    cuisines: ["American", "Snacks", "Turkish", "Portuguese", "Continental"],
-    avgRating: 4.4,
-    parentId: "371281",
-    avgRatingString: "4.4",
-    totalRatingsString: "10K+",
-    sla: {
-      deliveryTime: 25,
-      lastMileTravel: 1.4,
-      serviceability: "SERVICEABLE",
-      slaString: "25 mins",
-      lastMileTravelString: "1.4 km",
-      iconType: "ICON_TYPE_EMPTY",
-    },
-    availability: {
-      nextCloseTime: "2024-01-24 04:00:00",
-      opened: true,
-    },
-    badges: {},
-    isOpen: true,
-    type: "F",
-    badgesV2: {
-      entityBadges: {
-        imageBased: {},
-        textBased: {},
-        textExtendedBadges: {},
-      },
-    },
-    aggregatedDiscountInfoV3: {
-      header: "₹120 OFF",
-      subHeader: "ABOVE ₹199",
-      discountTag: "FLAT DEAL",
-    },
-    differentiatedUi: {
-      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-      differentiatedUiMediaDetails: {
-        mediaType: "ADS_MEDIA_ENUM_IMAGE",
-        lottie: {},
-        video: {},
-      },
-    },
-    reviewsSummary: {},
-    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-    restaurantOfferPresentationInfo: {},
-  },
-  analytics: {},
-  cta: {
-    link: "https://www.swiggy.com/restaurants/leons-burgers-and-wings-leon-grill-koramangala-bangalore-65797",
-    type: "WEBLINK",
-  },
 };
 
 const ResCard = (props) => {
@@ -117,7 +59,14 @@ const Body = () => {
         <button className="search-button">Search</button>
       </div>
       <div className="res-container">
-        <ResCard resData={resData} />
+        <ResCard resData={resList[0]} />
+        <ResCard resData={resList[1]} />
+        <ResCard resData={resList[2]} />
+        <ResCard resData={resList[3]} />
+        <ResCard resData={resList[5]} />
+        <ResCard resData={resList[6]} />
+        <ResCard resData={resList[7]} />
+        <ResCard resData={resList[8]} />
       </div>
     </div>
   );
