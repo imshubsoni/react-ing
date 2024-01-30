e4d0177b76aac23e027e59c0625987136a075a19
shubham-soni-amla <shubham.soni@amla.io>
2024-01-30 21:40:35 +0530

    Food Ordering App -- Using HOOKS -- Used hooks to create two buttons to filter restaurant based on rating, using State Variables

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,38 @@
 import ResCard from "./ResCard";
 import resList from "../utils/RestaurantsList";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
   return (
     <div className="body-container">
       <div className="search-container">
         <input className="search-box" placeholder="Search" name="search" />
         <button className="search-button">Search</button>
       </div>
+      <div className="filters-container">
+        <button
+          className="filter-button"
+          onClick={() => {
+            setListOfRestaurants(resList);
+          }}
+        >
+          All Restaurants
+        </button>
+        <button
+          className="filter-button"
+          onClick={() => {
+            setListOfRestaurants(
+              listOfRestaurants.filter((res) => res.info.avgRating > 4.3)
+            );
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurantInfo) => (
+        {listOfRestaurants.map((restaurantInfo) => (
           <ResCard key={restaurantInfo.info.id} resData={restaurantInfo} />
         ))}
       </div>
