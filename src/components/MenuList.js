bf9baaa1428228593692227beed9ae31ffea5c2a
Shubham Soni <imshubsoni@gmail.com>
2024-02-05 11:38:07 +0530

    Food Ordering App -- Show Price and Veg/NonVeg for each item

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -3,14 +3,22 @@ const MenuList = (cardInfo) => {
     cardInfo?.cardInfo?.card?.card?.title &&
     cardInfo?.cardInfo?.card?.card?.title != "Top Picks"
   ) {
-    const { itemCards } = cardInfo?.cardInfo?.card?.card;
+    const itemCards = cardInfo?.cardInfo?.card?.card?.itemCards
+      ? cardInfo?.cardInfo?.card?.card?.itemCards
+      : [];
 
     return (
       <div className="menulist-container">
         <h4>{cardInfo?.cardInfo?.card.card.title}</h4>
         <ul>
           {itemCards.map((item) => {
-            return <li>{item?.card?.info?.name}</li>;
+            return (
+              <li>
+                {item?.card?.info?.name} ---- Rs.{" "}
+                {item?.card?.info?.price / 100} ----{" "}
+                {item?.card?.info?.isVeg ? "VEG" : "NONVEG"}
+              </li>
+            );
           })}
         </ul>
       </div>
