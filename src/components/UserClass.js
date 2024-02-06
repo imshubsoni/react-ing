6015c3c439808b0ea74b71f09e9a780a2da62bfd
Shubham Soni <imshubsoni@gmail.com>
2024-02-06 14:48:52 +0530

    Food Ordering App -- Making API Calls in Class Based Component Using -- ComponentDidMount Lifecycle Method

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -7,24 +7,30 @@ class UserClass extends React.Component {
 
     this.state = {
       count: 0,
+      userInfo: {
+        name: "default",
+        location: "default",
+        avatar_url: "https://www.google.com/image1.jpg",
+      },
     };
   }
 
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/imshubsoni");
+
+    const json = await data.json();
+
+    this.setState({
+      userInfo: json,
+    });
+  }
+
   render() {
-    const { name, location } = this.props;
+    const { name, location, avatar_url } = this.state.userInfo;
 
     return (
       <div className="user-card">
-        <h4>Count: {this.state.count}</h4>
-        <button
-          onClick={() => {
-            this.setState({
-              count: ++this.state.count,
-            });
-          }}
-        >
-          Increase Count
-        </button>
+        <img src={avatar_url} />
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h3>Contact: @imshubsoni</h3>
