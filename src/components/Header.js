726279484700a92353e76cdea7ce28af542450db
shubham-soni-amla <shubham.soni@amla.io>
2024-02-09 01:23:41 +0530

    Food Ordering App -- installing & using Tailwind CSS -- styled header using tailwinf

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,29 +9,29 @@ const Header = () => {
   const onlineStatus = useOnlineStatus();
 
   return (
-    <div className="header-container">
+    <div className="header-container flex justify-between m-4 p-4 bg-green-200 rounded-lg shadow-zinc-400 shadow-lg sticky">
       <div className="logo-container">
-        <img className="logo" src={LOGO_URL}></img>
+        <img className="logo w-20" src={LOGO_URL}></img>
       </div>
-      <div className="nav-items-container">
-        <ul className="nav-items">
-          <li>Online Status: {onlineStatus ? "🌝" : "🌚"}</li>
-          <li>
+      <div className="nav-items-container flex items-center">
+        <ul className="nav-items flex">
+          <li className="px-4">Online Status: {onlineStatus ? "🌝" : "🌚"}</li>
+          <li className="px-4">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/grocerystore">Grocery</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/about">About Us</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/contact">Contact Us</Link>
           </li>
-          <li>Cart</li>
-          <li>
+          <li className="px-4">Cart</li>
+          <li className="px-4">
             <button
-              className="btn-login"
+              className="btn-login py-2 px-4 bg-slate-100 border border-solid border-black rounded-lg -mt-2 hover:bg-slate-200 hover:rounded-3xl transition-all ease-in-out duration-500"
               onClick={() => {
                 btnName === "Login"
                   ? setBtnName("Logout")
