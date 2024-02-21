c06ef48802223e2e359b6e8a8525c1f2e24c8542
imshubsoni <imshubsoni@gmail.com>
2024-02-21 11:05:23 +0530

    Food Ordering App -- Implementing Clear Cart Button -- REDUX

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,16 +1,31 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ItemCard from "./ItemCard";
+import { clearCart } from "../utils/reduxStore/storeSlices/cartSlice";
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart.items);
   console.log(cartItems);
 
+  const dispatch = useDispatch();
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+
   return (
     <div className="cart-container w-6/12 m-auto text-center">
       <h1 className="text-3xl font-bold m-4 p-4">Cart</h1>
-      <button className="border border-solid border-black p-1 rounded-lg w-3/12 hover:shadow-lg hover:cursor-pointer">
+      <button
+        className="border border-solid border-black p-1 rounded-lg w-3/12 hover:shadow-lg hover:cursor-pointer"
+        onClick={handleClearCart}
+      >
         Clear Cart
       </button>
+      {cartItems.length === 0 && (
+        <h1 className="text-xl mt-12 font-bold italic">
+          Cart is empty 😮 <br /> Please select food items from our vibrant menu
+          😍
+        </h1>
+      )}
       <div className="items-container mt-12">
         {cartItems.map((card) => (
           <ItemCard card={card} key={card.card.info.id} />
